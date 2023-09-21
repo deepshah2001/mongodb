8c8dbdbeba56396a9401df664ed49024a303c674
Koustubh Shah <90774217+deepshah2001@users.noreply.github.com>
2023-09-21 22:40:59 +0530

    User table

--- E-commerce App/models/user.js
+++ E-commerce App/models/user.js
@@ -1,16 +1,36 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const getDb = require('../util/database').getDb;
 
-const sequelize = require('../util/database');
+class User {
+  cosntructor(name, email, id) {
+    this.name = name;
+    this.email = email;
+    this._id = id;
+  }
 
-const User = sequelize.define('user', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  name: Sequelize.STRING,
-  email: Sequelize.STRING
-});
+  save() {
+    const db = getDb();
+    return db
+      .collection('users')
+      .insertOne(this)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch(err => console.log(err));
+  }
+
+  static findUserById(userId) {
+    const db = getDb();
+    return db
+      .collection('users')
+      .find({_id: new mongodb.ObjectId(userId)})
+      .next()
+      .then(user => {
+        console.log(user);
+        return user;
+      })
+      .catch(err => console.log(err));
+  }
+};
 
 module.exports = User;
