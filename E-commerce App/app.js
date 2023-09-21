4f5817a214cb5330dd61f8bb9e3256494de6862a
Koustubh Shah <90774217+deepshah2001@users.noreply.github.com>
2023-09-22 00:14:31 +0530

    Relations between tables

--- E-commerce App/app.js
+++ E-commerce App/app.js
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use((req, res, next) => {
   User.findUserById('650c7629c06cf0520a975530')
     .then(user => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch(err => console.log(err));
--- E-commerce App/models/user.js
+++ E-commerce App/models/user.js
@@ -1,36 +1,48 @@
-const mongodb = require('mongodb');
-const getDb = require('../util/database').getDb;
+const mongodb = require("mongodb");
+const getDb = require("../util/database").getDb;
 
 class User {
-  cosntructor(name, email, id) {
+  cosntructor(name, email, cart, id) {
     this.name = name;
     this.email = email;
+    this.cart = cart;
     this._id = id;
   }
 
   save() {
     const db = getDb();
     return db
-      .collection('users')
+      .collection("users")
       .insertOne(this)
       .then((result) => {
         console.log(result);
       })
-      .catch(err => console.log(err));
+      .catch((err) => console.log(err));
+  }
+
+  addToCart(product) {
+    const updatedCart = { items: [{ ...product, quantity: 1 }] };
+
+    const db = getDb();
+
+    return db.collection("users").updateOne(
+      { _id: new mongodb.ObjectId(this._id) },
+      { $set: { cart: updatedCart } }
+    );
   }
 
   static findUserById(userId) {
     const db = getDb();
     return db
-      .collection('users')
-      .find({_id: new mongodb.ObjectId(userId)})
+      .collection("users")
+      .find({ _id: new mongodb.ObjectId(userId) })
       .next()
-      .then(user => {
+      .then((user) => {
         console.log(user);
         return user;
       })
-      .catch(err => console.log(err));
+      .catch((err) => console.log(err));
   }
-};
+}
 
 module.exports = User;
