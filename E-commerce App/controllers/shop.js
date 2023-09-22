94cc28759dd50b5b36b4bf99f91ec689af38d11f
Koustubh Shah <90774217+deepshah2001@users.noreply.github.com>
2023-09-23 00:19:28 +0530

    Getting Orders Placed

--- E-commerce App/controllers/shop.js
+++ E-commerce App/controllers/shop.js
@@ -16,15 +16,6 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  // Product.findAll({ where: { id: prodId } })
-  //   .then(products => {
-  //     res.render('shop/product-detail', {
-  //       product: products[0],
-  //       pageTitle: products[0].title,
-  //       path: '/products'
-  //     });
-  //   })
-  //   .catch(err => console.log(err));
   Product.findById(prodId)
     .then((product) => {
       res.render("shop/product-detail", {
@@ -71,79 +62,22 @@ exports.postCart = (req, res, next) => {
       return req.user.addToCart(product);
     })
     .then((result) => console.log(result));
-  // let fetchedCart;
-  // let newQuantity = 1;
-
-  // .then((cart) => {
-  //   fetchedCart = cart;
-  //   return cart.getProducts({ where: { id: prodId } });
-  // })
-  // .then((products) => {
-  //   let product;
-  //   if (products.length > 0) {
-  //     product = products[0];
-  //   }
-
-  //   if (product) {
-  //     const oldQuantity = product.cartItem.quantity;
-  //     newQuantity = oldQuantity + 1;
-  //     return product;
-  //   }
-  //   return Product.findByPk(prodId);
-  // })
-  // .then((product) => {
-  //   return fetchedCart.addProduct(product, {
-  //     through: { quantity: newQuantity },
-  //   });
-  // })
-  // .then(() => {
-  //   
-  // })
-  // .catch((err) => console.log(err));
 };
 
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
   req.user
-    .getCart()
-    .then((cart) => {
-      return cart.getProducts({ where: { id: prodId } });
-    })
-    .then((products) => {
-      const product = products[0];
-      return product.cartItem.destroy();
-    })
-    .then((result) => {
+    .deleteCartItem(prodId)
+    .then(() => {
       res.redirect("/cart");
     })
     .catch((err) => console.log(err));
 };
 
 exports.postOrder = (req, res, next) => {
-  let fetchedCart;
   req.user
-    .getCart()
-    .then((cart) => {
-      fetchedCart = cart;
-      return cart.getProducts();
-    })
-    .then((products) => {
-      return req.user
-        .createOrder()
-        .then((order) => {
-          return order.addProducts(
-            products.map((product) => {
-              product.orderItem = { quantity: product.cartItem.quantity };
-              return product;
-            })
-          );
-        })
-        .catch((err) => console.log(err));
-    })
-    .then((result) => {
-      return fetchedCart.setProducts(null);
-    })
-    .then((result) => {
+    .getOrder()
+    .then(() => {
       res.redirect("/orders");
     })
     .catch((err) => console.log(err));
@@ -151,7 +85,7 @@ exports.postOrder = (req, res, next) => {
 
 exports.getOrders = (req, res, next) => {
   req.user
-    .getOrders({ include: ["products"] })
+    .getOrders()
     .then((orders) => {
       res.render("shop/orders", {
         path: "/orders",
